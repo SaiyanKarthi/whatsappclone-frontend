9a87f045e65b8473e2dcc201d4dc4f752cd3b76c
Saiyan Karthi <karthickeyand3v@gmail.com>
2025-12-01 17:44:45 +0530

    Deploy frontend for WhatsApp clone

--- src/firebox.js
+++ src/firebox.js
@@ -1,20 +1,17 @@
-
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlNFuVQD9sRt5ancn8S3EeIkcuRoJb3QQ",
-  authDomain: "mern-whatsapp-7b30f.firebaseapp.com",
-  projectId: "mern-whatsapp-7b30f",
-  storageBucket: "mern-whatsapp-7b30f.firebasestorage.app",
-  messagingSenderId: "96824971367",
-  appId: "1:96824971367:web:e01cce6d90004c317d84c2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
-
 const provider = new GoogleAuthProvider();
 
-export{app, auth, provider}
+export { app, auth, provider };
